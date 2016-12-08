e2f543eed2cbfa990c5002d50e68bb5a17bf8aa4
Durga Sai Preetham <durgasaipreethamp@yahoo.com>
2016-12-08 13:15:03 -0800

    updated objects chapter

--- object_literals.js
+++ object_literals.js
@@ -4,8 +4,11 @@ var person = {
     firstname: 'John',
     lastname: 'Doe',
     greet: function(){
-        console.log('Hello, ' + this.firstname + ' ' + this.lastname)
+        console.log('Hello, ' + this.firstname + ' ' + this.lastname);
     }
 }
 
 person.greet();
+// another way of accessing properties/methods of an object
+console.log(person['greet']);
+console.log(person['firstname']);
